c81cfe93edaa306f646d50269e70103a25da36ec
jnzhang622 <jnzhang622@gmail.com>
2021-01-17 03:35:55 -0500

    changed position of nominee names

--- src/Components/NomineeCards.js
+++ src/Components/NomineeCards.js
@@ -16,14 +16,16 @@ class NomineeCards extends React.Component{
 
   render(){
     return (
-      <div className="nomineeCard">
-          {/* <h5>{this.props.movie.Title} ({this.props.movie.Year})</h5> */}
-          <a href={`https://www.imdb.com/title/${this.props.movie.imdbID}`}>
-            <img src={this.props.movie.Poster} 
-              width='80px'
-              alt={this.props.movie.Title}/>
-          </a>
-          <button onClick={this.handleDelete}>Remove from Nominees</button>
+      <div>
+        <a>{this.props.movie.Title} ({this.props.movie.Year})</a>
+        <div className="nomineeCard">
+            <a href={`https://www.imdb.com/title/${this.props.movie.imdbID}`}>
+              <img src={this.props.movie.Poster} 
+                width='80px'
+                alt={this.props.movie.Title}/>
+            </a>
+            <button className="defaultButton" onClick={this.handleDelete}>Remove from Nominees</button>
+        </div>
       </div>
     )
   }
